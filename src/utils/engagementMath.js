3f2b602a7ed917a0535eebf392893e12e6e8b8c6
Liyam Flx <135199157+LiyamFlx@users.noreply.github.com>
2025-01-16 23:56:10 -0800

    Update engagementMath.js

--- src/utils/engagementMath.js
+++ src/utils/engagementMath.js
@@ -1,1 +1,8 @@
-// Logic for Engagement Positioning Placeholder
+export const calculateDotPosition = (engagement) => {
+  const { physical, emotional, mental, spiritual } = engagement;
+
+  const x = (emotional - physical) * 0.5;
+  const y = (spiritual - mental) * 0.5;
+
+  return { x: 50 + x, y: 50 - y };
+};
