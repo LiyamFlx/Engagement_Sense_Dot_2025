424b1ac55fe8188ffda16c77c476b2db902bb051
Liyam Flx <135199157+LiyamFlx@users.noreply.github.com>
2025-01-16 23:53:06 -0800

    Update Circle.jsx

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,1 +1,19 @@
-// Circle Component Code Placeholder
+import React from "react";
+import "./circle.css";
+import Dot from "./Dot";
+
+const Circle = ({ engagementData }) => {
+  return (
+    <div className="circle-container">
+      <div className="circle">
+        <div className="quadrant physical">Physical</div>
+        <div className="quadrant emotional">Emotional</div>
+        <div className="quadrant mental">Mental</div>
+        <div className="quadrant spiritual">Spiritual</div>
+        <Dot engagementData={engagementData} />
+      </div>
+    </div>
+  );
+};
+
+export default Circle;
