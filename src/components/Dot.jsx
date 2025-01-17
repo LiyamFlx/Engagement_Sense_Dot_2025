d6502d47a0766342eb4fca055dbed8d0f9a05c36
Liyam Flx <135199157+LiyamFlx@users.noreply.github.com>
2025-01-16 23:53:47 -0800

    Update Dot.jsx

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,1 +1,23 @@
-// Dot Component Code Placeholder
+import React, { useEffect, useState } from "react";
+import { calculateDotPosition } from "../utils/engagementMath";
+import "./dot.css";
+
+const Dot = ({ engagementData }) => {
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+
+  useEffect(() => {
+    setPosition(calculateDotPosition(engagementData));
+  }, [engagementData]);
+
+  return (
+    <div
+      className="dot"
+      style={{
+        left: `${position.x}%`,
+        top: `${position.y}%`,
+      }}
+    ></div>
+  );
+};
+
+export default Dot;
